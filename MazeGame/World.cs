83a6f461513a4fab1dc04c2dc0395e17ce0d7c9c
Logan Silvers <silvers@colum.edu>
2024-10-21 21:13:24 -0500

    Add files via upload

---
+++ MazeGame/World.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MazeGame
+{
+    internal class World
+    {
+        private string[,] Grid;
+        private int Rows;
+        private int Cols;
+
+        public World(string[,] grid) 
+        {
+            Grid = grid;
+            Rows = Grid.GetLength(0);
+            Cols = Grid.GetLength(1);
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    string str = Grid[i, j];
+                    SetCursorPosition(j, i);
+                    if (str == "X")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Write(str);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Write(str);
+                    }
+                }
+            }
+        }
+
+        public string getThing(int x, int y)
+        {
+            return Grid[y, x];
+        }
+
+        public bool IsWalkable(int x, int y)
+        {
+            if (x < 0 || y < 0 || x>= Cols || y >= Rows) return false;
+
+            return Grid[y, x] == " " || Grid[y, x] == "X";
+
+        }
+    }
+}
